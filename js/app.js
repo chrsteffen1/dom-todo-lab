25a7e885350a32d2659fa2c61fe934fe1086dd54
Chrsteffen <Chrsteffen1@gmail.com>
2022-08-31 17:28:13 -0400

    add alert on empty input

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ const uList = document.getElementById('todo-list')
 button.addEventListener('click' , (evt) => {
   const li = document.createElement('li')
   li.textContent = input.value
+  if (input.value === ''){
+    window.alert("You can't add nothing to your TO DO list")
+  } else {
   document.querySelector('ul').appendChild(li)
   input.value = ''
+  }
 })
