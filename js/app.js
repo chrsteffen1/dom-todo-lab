55e45cce44b57d9c0cd69abea74999f26da708cb
Chrsteffen <Chrsteffen1@gmail.com>
2022-08-31 10:19:26 -0400

    add items to list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('sanity check');
+const input = document.getElementById('enterTxt')
+const button = document.getElementById('submit-button')
+const uList = document.getElementById('todo-list')
+
+button.addEventListener('click' , (evt) => {
+  const li = document.createElement('li')
+  li.textContent = input.value
+  document.querySelector('ul').appendChild(li)
+})
